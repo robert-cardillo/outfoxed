f6daced8f8ce895c9ac61dcebdd55f417e60cf79
robert-cardillo <robert-cardillo@users.noreply.github.com>
2023-01-23 23:22:26 +0300

    react-screen-wake-lock

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 import "./App.css";
 import Chooser from "./components/Chooser";
 import Roller from "./components/Roller";
+import { useWakeLock } from "react-screen-wake-lock";
 
 function App() {
   const [state, setState] = useState("choose"); // choose | roll | fail | success
   const [chioce, setChioce] = useState(""); // paw | eye
+  const { isSupported, released, request, release } = useWakeLock({
+    onError: () => alert("Screen Wake Lock: error!"),
+    onRelease: () => alert("Screen Wake Lock: released!"),
+  });
+  request();
 
   return (
     <div className="app">
